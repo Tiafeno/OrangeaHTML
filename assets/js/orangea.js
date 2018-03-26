ff5370a4511b439a30f40aa27eff835348a9e93e
Tiafeno <tiafenofnel@gmail.com>
2018-03-26 17:28:05 +0300

    First commit html template

---
+++ assets/js/orangea.js
@@ -0,0 +1,42 @@
+(function ($) {
+  'use strict';
+  $(document).ready(function () {
+    var org_bg_top = $('.__org_bg_top'),
+      org_bg_bottom = $('.__org_bg_bottom'),
+      backgroundPosition = function () {
+        /**
+         * Positionner le fond en haut
+         */
+        org_bg_top.each(function (index, el) {
+          var elementHeight = $(el).height();
+          $(el).css({
+            top: -elementHeight + "px",
+            "margin-bottom": -elementHeight + "px"
+          });
+        });
+        /**
+         * Positionner le fond en bas
+         */
+        org_bg_bottom.each(function (index, el) {
+          var elementHeight = $(el).height();
+          $(el).css({
+            top: elementHeight + "px",
+            "margin-top": -elementHeight + "px"
+          });
+        });
+      },
+
+      /**
+       * Constructeur
+       * @private
+       */
+      __init__ = function () {
+        backgroundPosition();
+      };
+
+    __init__();
+    $(window).resize(function () {
+      backgroundPosition();
+    });
+  });
+})( jQuery );

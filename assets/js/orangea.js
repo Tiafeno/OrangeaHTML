80d5d1d27b551b697b457ffdafc1dac02f16b8b7
Tiafeno <tiafenofnel@gmail.com>
2018-04-06 13:27:29 +0300

    fix viewport and add excursion svg

--- assets/js/orangea.js
+++ assets/js/orangea.js
@@ -1,38 +1,37 @@
 (function ($) {
   'use strict';
   $(document).ready(function () {
-    var bgTop = $('.__org_bg_top'),
-      bgBottom = $('.__org_bg_bottom'),
-      backgroundPosition = function () {
-        /**
-         * Positionner le fond en haut
-         */
-        bgTop.each(function (index, el) {
-          var elementHeight = $(el).height();
-          $(el).css({
-            top: -elementHeight + "px",
-            "margin-bottom": -elementHeight + "px"
-          });
-        });
-        /**
-         * Positionner le fond en bas
-         */
-        bgBottom.each(function (index, el) {
-          var elementHeight = $(el).height();
-          $(el).css({
-            top: elementHeight + "px",
-            "margin-top": -elementHeight + "px"
-          });
-        });
-      },
-
+    var bgTop = $('.__org_bg_top');
+    var bgBottom = $('.__org_bg_bottom');
+    var backgroundPosition = function () {
       /**
-       * Constructeur
-       * @private
+       * Positionner le fond en haut
        */
-      __init__ = function () {
-        backgroundPosition();
-      };
+      bgTop.each(function (index, el) {
+        var elementHeight = $(el).height();
+        $(el).css({
+          top: -elementHeight + "px",
+          "margin-bottom": -elementHeight + "px"
+        });
+      });
+      /**
+       * Positionner le fond en bas
+       */
+      bgBottom.each(function (index, el) {
+        var elementHeight = $(el).height();
+        $(el).css({
+          top: elementHeight + "px",
+          "margin-top": -elementHeight + "px"
+        });
+      });
+    };
+    /**
+     * Constructeur
+     * @private
+     */
+    var __init__ = function () {
+      backgroundPosition();
+    };
 
     __init__();
     $(window).resize(function () {
